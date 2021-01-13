30434e7a09fc82791a6dd17345d01d91f25eb1bf
Zoe Kirsh <zoekirsh@gmail.com>
2021-01-12 22:24:36 -0800

    all passing

--- index.js
+++ index.js
@@ -10,4 +10,18 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(strTime) {
+  let timeArr = strTime.split(':');
+  let hours = parseInt(timeArr[0]);
+  if (hours < 12 ) {
+    return "Good Morning"
+  } else if (hours > 12 && hours < 17) {
+    return "Good Afternoon"
+  } else {
+    return "Good Evening"
+  };
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(arg) {
+  document.getElementById('greeting').innerText = arg
+}
